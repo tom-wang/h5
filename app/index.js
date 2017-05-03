f24ec5ecc24032272ce8822e28990190ca7635d5
tomszwang <tom.wang.sz@qq.com>
2017-05-03 14:18:17 +0800

    提交修改

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import test from 'test';
 import test1 from 'test1';
-import './common.css';
 
 console.log('index');
+test.say();
--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,27 @@ module.exports = {
     },
     output: {
         //filename: '[name].[chunkhash].js',
-        filename: 'bundle.js',
+        //filename: 'bundle_[hash]_[chunkhash].js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
-        rules: [{
-            test: /\.css$/,
-            use: 'css-loader'
-        }]
+        rules: [
+            {
+                test: /\.css$/,
+                use: 'css-loader'
+            },
+            {
+                test: /\.js$/,
+                //exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
    },
     /*
    plugins: [
